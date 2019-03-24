327011e2ddd36acb74ceaba85d08317c3b9eb4e3
Julian Todd <julian@goatchurch.org.uk>
2019-03-24 20:26:29 +0000

    allow to connect between two active phone hotspots

--- esp32code/UDPblackviewphone.py
+++ esp32code/UDPblackviewphone.py
@@ -4,25 +4,22 @@ sockudp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sockudp.settimeout(0.02)
 si = network.WLAN(network.STA_IF) 
 
-# Connect to Blackview (Android) phone
-def connectBlackview(pled):
-    wconn, wpass = b'BV6000', b'beckaaaa'
+# Connect to Blackview/S5 (Android) phone
+def connectActivePhone(pled):
     si.active(True)
-    bwconnexists = bool(l  for l in si.scan()  if l[0] == wconn)
+    ssids = [ l[0]  for l in si.scan() ]
+    if b'BV6000' in ssids:
+        wconn, wpass = b'BV6000', b'beckaaaa'
+    elif b'ES_3041' in ssids:
+        wconn, wpass = b'ES_3041', b'43900000'
+    else:
+        return False
     si.connect(wconn, wpass)
     while not si.isconnected():
         time.sleep_ms(100)
         pled.value(1-pled.value())
+    return True
 
-def connectS5(pled):
-    wconn, wpass = b'ES_3041', b'43900000'
-    si.active(True)
-    bwconnexists = bool(l  for l in si.scan()  if l[0] == wconn)
-    si.connect(wconn, wpass)
-    while not si.isconnected():
-        time.sleep_ms(100)
-        pled.value(1-pled.value())
-        
 # udpaddr = ("192.168.43.1", 9019)  # ip default for android
 secondaddr = None
 def dwrite(mess, udpaddr):
