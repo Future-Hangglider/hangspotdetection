1fd67273cc67604184992aae9dac2a2e90a60f9e
Julian Todd <julian@goatchurch.org.uk>
2019-04-28 19:55:38 +0100

    add hotspot file for connections

--- esp32code/UDPblackviewphone.py
+++ esp32code/UDPblackviewphone.py
@@ -6,6 +6,12 @@ si = network.WLAN(network.STA_IF)
 
 # Connect to Blackview/S5 (Android) phone
 def connectActivePhone(pled):
+    hotspots = { }
+    for l in open("hotspots.txt", "rb"):
+        s = l.split()
+        hotspots[s[0]] = (s[1], s[2].decode(), int(s[3]))
+    print(hotspots)
+    
     si.active(True)
     siscanned = si.scan()
     siscanned.sort(key=lambda X:X[3])
@@ -13,19 +19,17 @@ def connectActivePhone(pled):
         wc = siscanned.pop()
         print(wc)
         wconn = wc[0]
-        if wconn == b'BV6000':
-            wpass = b'beckaaaa'
+        if wconn in hotspots:
+            wpass, host, port = hotspots[wconn]
             break
-        if wconn == b'ES_3041':
-            wpass = b'43900000'
-            break
+            
     else:
-        return False
+        return None, None
     si.connect(wconn, wpass)
     while not si.isconnected():
         time.sleep_ms(100)
         pled.value(1-pled.value())
-    return True
+    return host, port
 
 # udpaddr = ("192.168.43.1", 9019)  # ip default for android
 secondaddr = None
